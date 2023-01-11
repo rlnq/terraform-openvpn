cffae2a22867647e20714acc8c05663f3476bb1d
rlnq <117667360+rlnq@users.noreply.github.com>
2023-01-11 16:20:13 +0200

    Update user_data.sh

--- user_data.sh
+++ user_data.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#whakaaaro
 
 get_public_ip=$(grep -m 1 -oE '^[0-9]{1,3}(\.[0-9]{1,3}){3}$' <<< "$(wget -T 10 -t 1 -4qO- "http://ip1.dynupdate.no-ip.com/" || curl -m 10 -4Ls "http://ip1.dynupdate.no-ip.com/")")
 
